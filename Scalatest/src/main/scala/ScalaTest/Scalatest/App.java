9b17944b2fcfef5f033e9d15b9831af0677a3beb
venkatms578 <venkatms578@gmail.com>
2018-04-18 23:02:42 -0400

    hello

---
+++ Scalatest/src/main/scala/ScalaTest/Scalatest/App.java
@@ -0,0 +1,13 @@
+package ScalaTest.Scalatest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
